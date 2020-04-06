c7c6c8906ba4d006986b2fd25e099a8664188de8
ryan-nanson <ryan_nanson@hotmail.com>
2020-04-06 18:26:46 +0100

    WIP make number of dice dynamic

--- app/src/main/java/com/ryryapps/MainActivity.kt
+++ app/src/main/java/com/ryryapps/MainActivity.kt
@@ -2,13 +2,12 @@ package com.ryryapps
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.ListPreference
 import androidx.preference.PreferenceManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ryryapps.dice.R
@@ -31,31 +30,22 @@ class MainActivity : AppCompatActivity() {
         val dice1ImageView = findViewById<ImageView>(R.id.dice1ImageView)
         val dice2ImageView = findViewById<ImageView>(R.id.dice2ImageView)
 
-        val seekBar = findViewById<SeekBar>(R.id.seekBar)
-        val howManyTextView = findViewById<TextView>(R.id.howManyTextView)
+        val howManyTextView = findViewById<TextView>(R.id.TotalTextView)
 
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.applicationContext)
-
-        var numberOfDice = sharedPreferences.getString("how_may_dice", "1")
 
         var dice1 = sharedPreferences.getString("dice_1", "6")
         var dice2 = sharedPreferences.getString("dice_2", "6")
         diceDescriptionTextView.text = "d" + dice1
         dice2DescriptionTextView.text = "d" + dice2
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                // Display the current progress of SeekBar
-                val j = i + 1
-                howManyTextView.text = "Dice size: $j"
-            }
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-                // Do something
-            }
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                // Do something
-            }
-        })
+        var numberOfDice = sharedPreferences.getString("how_many_dice", "1")
+
+        if (numberOfDice == "1") {
+            dice2TextView.text = ""
+            dice2DescriptionTextView.text = ""
+            dice2ImageView.visibility = View.INVISIBLE
+        }
 
         rollButton.setOnClickListener() {
 
@@ -64,20 +54,23 @@ class MainActivity : AppCompatActivity() {
             // Animate dice shake action.
             dice1TextView.startAnimation(shake)
             dice1ImageView.startAnimation(shake)
-            dice2TextView.startAnimation(shake)
-            dice2ImageView.startAnimation(shake)
 
             // Set random number on dice.
             val dice1 = Random().nextInt(dice1!!.toInt()) + 1
-            val dice2 = Random().nextInt(seekBar.progress + 1) + 1
             dice1TextView.text = dice1.toString()
-            dice2TextView.text = dice2.toString()
+
+            if (numberOfDice!!.toInt() >= 2) {
+                dice2TextView.startAnimation(shake)
+                dice2ImageView.startAnimation(shake)
+
+                val dice2 = Random().nextInt(dice2!!.toInt()) + 1
+                dice2TextView.text = dice2.toString()
+            }
         }
 
         settingsButton.setOnClickListener() {
             val intent = Intent(this, SettingsActivity::class.java).apply{}
             startActivity(intent)
         }
-
     }
 }
