6a278926a64ef59665b02daa55416369f29efafc
ryan-nanson <ryan_nanson@hotmail.com>
2020-04-07 17:13:30 +0100

    feat configure to use multiple dice dynamically

--- app/src/main/java/com/ryryapps/MainActivity.kt
+++ app/src/main/java/com/ryryapps/MainActivity.kt
@@ -24,30 +24,69 @@ class MainActivity : AppCompatActivity() {
 
         val dice1TextView = findViewById<TextView>(R.id.dice1TextView)
         val dice2TextView = findViewById<TextView>(R.id.dice2TextView)
-        val diceDescriptionTextView = findViewById<TextView>(R.id.diceDescriptionTextView)
+        val dice3TextView = findViewById<TextView>(R.id.dice3TextView)
+        val dice4TextView = findViewById<TextView>(R.id.dice4TextView)
+
+        val dice1DescriptionTextView = findViewById<TextView>(R.id.dice1DescriptionTextView)
         val dice2DescriptionTextView = findViewById<TextView>(R.id.dice2DescriptionTextView)
+        val dice3DescriptionTextView = findViewById<TextView>(R.id.dice3DescriptionTextView)
+        val dice4DescriptionTextView = findViewById<TextView>(R.id.dice4DescriptionTextView)
 
         val dice1ImageView = findViewById<ImageView>(R.id.dice1ImageView)
         val dice2ImageView = findViewById<ImageView>(R.id.dice2ImageView)
+        val dice3ImageView = findViewById<ImageView>(R.id.dice3ImageView)
+        val dice4ImageView = findViewById<ImageView>(R.id.dice4ImageView)
 
-        val howManyTextView = findViewById<TextView>(R.id.TotalTextView)
+        val totalTextView = findViewById<TextView>(R.id.TotalTextView)
 
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.applicationContext)
 
-        var dice1 = sharedPreferences.getString("dice_1", "6")
-        var dice2 = sharedPreferences.getString("dice_2", "6")
-        diceDescriptionTextView.text = "d" + dice1
-        dice2DescriptionTextView.text = "d" + dice2
+        val dice1Size = sharedPreferences.getString("dice_1", "6")
+        val dice2Size = sharedPreferences.getString("dice_2", "6")
+        val dice3Size = sharedPreferences.getString("dice_3", "6")
+        val dice4Size = sharedPreferences.getString("dice_4", "6")
 
-        var numberOfDice = sharedPreferences.getString("how_many_dice", "1")
+        dice1DescriptionTextView.text = "d$dice1Size"
+        dice2DescriptionTextView.text = "d$dice2Size"
+        dice3DescriptionTextView.text = "d$dice3Size"
+        dice4DescriptionTextView.text = "d$dice4Size"
 
-        if (numberOfDice == "1") {
-            dice2TextView.text = ""
-            dice2DescriptionTextView.text = ""
-            dice2ImageView.visibility = View.INVISIBLE
+        val numberOfDice = sharedPreferences.getString("how_many_dice", "1")
+
+        when (numberOfDice) {
+            "1" -> {
+                dice2TextView.text = ""
+                dice2DescriptionTextView.text = ""
+                dice2ImageView.visibility = View.INVISIBLE
+
+                dice3TextView.text = ""
+                dice3DescriptionTextView.text = ""
+                dice3ImageView.visibility = View.INVISIBLE
+
+                dice4TextView.text = ""
+                dice4DescriptionTextView.text = ""
+                dice4ImageView.visibility = View.INVISIBLE
+            }
+            "2" -> {
+                dice3TextView.text = ""
+                dice3DescriptionTextView.text = ""
+                dice3ImageView.visibility = View.INVISIBLE
+
+                dice4TextView.text = ""
+                dice4DescriptionTextView.text = ""
+                dice4ImageView.visibility = View.INVISIBLE
+            }
+            "3" -> {
+                dice4TextView.text = ""
+                dice4DescriptionTextView.text = ""
+                dice4ImageView.visibility = View.INVISIBLE
+            }
+
+            else -> { // Number of dice == 4
+            }
         }
 
-        rollButton.setOnClickListener() {
+        rollButton.setOnClickListener {
 
             val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
 
@@ -56,16 +95,39 @@ class MainActivity : AppCompatActivity() {
             dice1ImageView.startAnimation(shake)
 
             // Set random number on dice.
-            val dice1 = Random().nextInt(dice1!!.toInt()) + 1
-            dice1TextView.text = dice1.toString()
+            val dice1Value = Random().nextInt(dice1Size!!.toInt()) + 1
+            var total = dice1Value
+            dice1TextView.text = dice1Value.toString()
 
             if (numberOfDice!!.toInt() >= 2) {
                 dice2TextView.startAnimation(shake)
                 dice2ImageView.startAnimation(shake)
 
-                val dice2 = Random().nextInt(dice2!!.toInt()) + 1
-                dice2TextView.text = dice2.toString()
+                val dice2Value = Random().nextInt(dice2Size!!.toInt()) + 1
+                dice2TextView.text = dice2Value.toString()
+
+                total += dice2Value
             }
+            if (numberOfDice.toInt() >= 3) {
+                dice3TextView.startAnimation(shake)
+                dice3ImageView.startAnimation(shake)
+
+                val dice3Value = Random().nextInt(dice3Size!!.toInt()) + 1
+                dice3TextView.text = dice3Value.toString()
+
+                total += dice3Value
+            }
+            if (numberOfDice.toInt() == 4) {
+                dice4TextView.startAnimation(shake)
+                dice4ImageView.startAnimation(shake)
+
+                val dice4Value = Random().nextInt(dice4Size!!.toInt()) + 1
+                dice4TextView.text = dice4Value.toString()
+
+                total += dice4Value
+            }
+
+            totalTextView.text = "Total: $total"
         }
 
         settingsButton.setOnClickListener() {
