3b8517b96453cddf028791df6eeb18120506c655
ryan-nanson <ryan_nanson@hotmail.com>
2020-04-08 17:55:48 +0100

    add themes configuration

--- app/src/main/java/com/ryryapps/MainActivity.kt
+++ app/src/main/java/com/ryryapps/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ryryapps.dice.R
@@ -40,6 +42,10 @@ class MainActivity : AppCompatActivity() {
         val totalTextView = findViewById<TextView>(R.id.TotalTextView)
 
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.applicationContext)
+
+        val theme = sharedPreferences.getString("appearance_theme", "casino")
+
+        updateColorScheme(theme)
 
         val dice1Size = sharedPreferences.getString("dice_1", "6")
         val dice2Size = sharedPreferences.getString("dice_2", "6")
@@ -81,7 +87,6 @@ class MainActivity : AppCompatActivity() {
                 dice4DescriptionTextView.text = ""
                 dice4ImageView.visibility = View.INVISIBLE
             }
-
             else -> { // Number of dice == 4
             }
         }
@@ -134,5 +139,77 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SettingsActivity::class.java).apply{}
             startActivity(intent)
         }
+
+    }
+
+    private fun updateColorScheme(theme: String?) {
+
+        val backgroundLayout = findViewById<ConstraintLayout>(R.id.backgroundLayout)
+
+        val dice1ImageView = findViewById<ImageView>(R.id.dice1ImageView)
+        val dice2ImageView = findViewById<ImageView>(R.id.dice2ImageView)
+        val dice3ImageView = findViewById<ImageView>(R.id.dice3ImageView)
+        val dice4ImageView = findViewById<ImageView>(R.id.dice4ImageView)
+
+        when (theme) {
+            "casino" -> {
+                backgroundLayout.setBackgroundColor(ContextCompat.getColor(this.applicationContext, R.color.casinoBackground))
+                dice1ImageView.setImageResource(R.drawable.dice_red)
+                dice2ImageView.setImageResource(R.drawable.dice_red)
+                dice3ImageView.setImageResource(R.drawable.dice_red)
+                dice4ImageView.setImageResource(R.drawable.dice_red)
+                updateTextColor(ContextCompat.getColor(this.applicationContext, R.color.casinoText))
+            }
+            "light" -> {
+                backgroundLayout.setBackgroundColor(ContextCompat.getColor(this.applicationContext, R.color.lightBackground))
+                dice1ImageView.setImageResource(R.drawable.dice)
+                dice2ImageView.setImageResource(R.drawable.dice)
+                dice3ImageView.setImageResource(R.drawable.dice)
+                dice4ImageView.setImageResource(R.drawable.dice)
+                updateTextColor(ContextCompat.getColor(this.applicationContext, R.color.lightText))
+            }
+            "dark" -> {
+                backgroundLayout.setBackgroundColor(ContextCompat.getColor(this.applicationContext, R.color.darkBackground))
+                dice1ImageView.setImageResource(R.drawable.dice)
+                dice2ImageView.setImageResource(R.drawable.dice)
+                dice3ImageView.setImageResource(R.drawable.dice)
+                dice4ImageView.setImageResource(R.drawable.dice)
+                updateTextColor(ContextCompat.getColor(this.applicationContext, R.color.darkText))
+            }
+            else -> { // default to casino
+                backgroundLayout.setBackgroundColor(ContextCompat.getColor(this.applicationContext, R.color.casinoBackground))
+                dice1ImageView.setImageResource(R.drawable.dice_red)
+                dice2ImageView.setImageResource(R.drawable.dice_red)
+                dice3ImageView.setImageResource(R.drawable.dice_red)
+                dice4ImageView.setImageResource(R.drawable.dice_red)
+                updateTextColor(ContextCompat.getColor(this.applicationContext, R.color.casinoText))
+            }
+        }
+    }
+
+    private fun updateTextColor(textColor: Int) {
+        val dice1TextView = findViewById<TextView>(R.id.dice1TextView)
+        val dice2TextView = findViewById<TextView>(R.id.dice2TextView)
+        val dice3TextView = findViewById<TextView>(R.id.dice3TextView)
+        val dice4TextView = findViewById<TextView>(R.id.dice4TextView)
+
+        val dice1DescriptionTextView = findViewById<TextView>(R.id.dice1DescriptionTextView)
+        val dice2DescriptionTextView = findViewById<TextView>(R.id.dice2DescriptionTextView)
+        val dice3DescriptionTextView = findViewById<TextView>(R.id.dice3DescriptionTextView)
+        val dice4DescriptionTextView = findViewById<TextView>(R.id.dice4DescriptionTextView)
+
+        val totalTextView = findViewById<TextView>(R.id.TotalTextView)
+
+        dice1TextView.setTextColor(textColor)
+        dice2TextView.setTextColor(textColor)
+        dice3TextView.setTextColor(textColor)
+        dice4TextView.setTextColor(textColor)
+
+        dice1DescriptionTextView.setTextColor(textColor)
+        dice2DescriptionTextView.setTextColor(textColor)
+        dice3DescriptionTextView.setTextColor(textColor)
+        dice4DescriptionTextView.setTextColor(textColor)
+
+        totalTextView.setTextColor(textColor)
     }
 }
